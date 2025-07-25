ff423b1e259bc74ed1732abe55e3d96aeb24b216
gastii11 <10632@holbertonstudents.com>
2025-07-25 20:09:02 -0300

    0

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """se sumaran dos flotantes"""
 
-def add(self, a: float, b: float) -> float:
+def add(a: float, b: float) -> float:
     """devuelve la suma"""
     return a + b
