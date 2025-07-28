73c29da579d4404f7fb2e2ff61c86618b1f6b31c
gastii11 <10632@holbertonstudents.com>
2025-07-28 15:14:56 -0300

    ejercicio 4

---
+++ python_variable_annotations/4-define_variables.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+"""
+a, un entero con un valor de 1.
+pi, un flotante con un valor de 3,14.
+i_understand_annotations, un booleano con un valor de True.
+school, una cadena con un valor de «Holberton».
+"""
+
+
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = "Holberton"
