b32d300356ee34fd8d459d65865e3560427b0f21
gastii11 <10632@holbertonstudents.com>
2025-07-28 20:18:13 -0300

    ejercicio 1

---
+++ python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+'''
+Función asíncrona que ejecuta múltiples llamadas a wait_random
+y devuelve los retardos en orden ascendente sin usar sort().
+'''
+from typing import List
+import asyncio
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    '''
+    Ejecutamos todas las tareas y las procesamos conforme terminan
+    '''
+    delays = []
+    for task in asyncio.as_completed([wait_random(max_delay) for _ in range(n)]):
+        delay = await task
+        delays.append(delay)
+    return delays
