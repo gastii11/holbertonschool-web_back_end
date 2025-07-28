fb658f4ccdd4c0a1ce488308556a0d4bf64b3be3
gastii11 <10632@holbertonstudents.com>
2025-07-28 15:06:06 -0300

    ejercicio 3

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+Función anotada con tipo to_str que tome un número flotante n
+como argumento y devuelva la representación en cadena del número flotante
+"""
+
+
+def to_str(n: float) -> str:
+    """n será str"""
+    return str(n)
