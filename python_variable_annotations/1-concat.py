d3092f9adf2a5138b3e53c30ed9c348899609c61
gastii11 <10632@holbertonstudents.com>
2025-07-28 14:01:45 -0300

    ejercicio 1

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+'''
+Este módulo es para concatenar dos cadenas.
+'''
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    Concatenar dos cadenas.
+    """
+    return str1 + str2
